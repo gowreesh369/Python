e8b800002a2cf5d10f9b5c5cca124a21d1a1f36b
gowreesh369 <143312648+gowreesh369@users.noreply.github.com>
2023-11-15 22:13:23 +0530

    Add files via upload

---
+++ fundamentals/variables.py
@@ -0,0 +1,17 @@
+name  = "Gowreesh"
+#print(name)
+#print(type(name))
+firstName = "Max"
+lastName = "Verstappen"
+fullname = firstName + " " + lastName
+#print(fullname)
+age = 26
+#print("Age is: " + str(age))
+name = "Messi"
+human = False
+#print(name + " is a human: " + str(human))
+#Multiple assignments
+name, age, goat = "Messi", "36", True
+print(name)
+print(age)
+print(goat)
