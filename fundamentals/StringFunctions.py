784932c883bcd76298b62a3f4f67461ebb248ea8
gowreesh369 <143312648+gowreesh369@users.noreply.github.com>
2023-11-15 22:13:08 +0530

    Add files via upload

---
+++ fundamentals/StringFunctions.py
@@ -0,0 +1,9 @@
+name = "Messi"
+#print(len(name))
+#print(name.find("M"))
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("M"))
+print(name.replace("i", "s"))
