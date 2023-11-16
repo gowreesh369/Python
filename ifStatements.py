e231b6edcfd0fedcde9785994f9dd9aa0a70b66b
gowreesh369 <gowreesh369@gmail.com>
2023-11-16 22:15:40 +0530

    Add files via upload

---
+++ ifStatements.py
@@ -0,0 +1,11 @@
+age  = int(input("Enter your age: "))
+if age>=18: {
+   print("You are an adult")
+}
+elif age<0: {
+        print("Not yet born")
+    }
+else: {
+    print("You are a child")
+}
+ 
