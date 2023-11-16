f4c17388e5b8b96cc22e2299de2379fe81dada77
gowreesh369 <gowreesh369@gmail.com>
2023-11-16 22:14:41 +0530

    Add files via upload

---
+++ fundamentals/mathFunctions.py
@@ -0,0 +1,13 @@
+import math
+pi = 3.14
+a = 5
+b =1
+c = 99
+#print(round(pi))
+#print(math.floor(pi))
+#print(math.ceil(pi))
+#print(abs(pi))
+#print(pow(pi,2))
+#print(math.sqrt(pi))
+#print(max(pi,a,b,c))
+#print(min(pi,a,b,c))
