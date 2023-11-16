4edad52a7872dd6a1facd24a14084baad742f6cd
gowreesh369 <gowreesh369@gmail.com>
2023-11-16 22:14:10 +0530

    Add files via upload

---
+++ fundamentals/input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = int(input("What is your age? "))
+height = float(input("What is your height? "))
+
+print("You are "+ name)
+print("You are "+ str(age)+ " years old")
+print("You are " + str(height)+ " cm tall")
