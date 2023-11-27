2a6869c547ccaa641f0789d58abc12cc451b6838
gowreesh369 <gowreesh369@gmail.com>
2023-11-27 21:01:10 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,10 @@
+nums = {1,2,3,3}
+nums2 = {3,5,7,9}
+#nums.add(5)
+#print(nums)
+#nums2.update(nums)
+#print(nums2)
+#ans = nums2.union(nums)
+
+#print(nums.difference(nums2))
+print(nums.intersection(nums2))
